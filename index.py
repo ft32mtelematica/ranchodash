d9ae029e023564c811e7341e616fc7da10e4b8d4
ft32mtelematica <ft32mtelematica@gmail.com>
2025-07-25 19:22:29 -0300

    Update index.py
    Alteração no caminho das páginas

--- index.py
+++ index.py
@@ -2,9 +2,9 @@ import streamlit as st
 st.title("PREVISÃO DE RANCHO")
 
 pg = st.navigation([
-    st.Page("pages/geral.py", title="Valores Diários", icon="📊"),
-    st.Page("pages/por_pessoa2.py", title="Valores por Pessoa", icon="👤"),
-    st.Page("pages/fluxodecaixa.py", title="Fluxo de Caixa", icon=("💰")),
+    st.Page("geral.py", title="Valores Diários", icon="📊"),
+    st.Page("por_pessoa2.py", title="Valores por Pessoa", icon="👤"),
+    st.Page("fluxodecaixa.py", title="Fluxo de Caixa", icon=("💰")),
 ])
 
 pg.run()

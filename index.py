adcca27de44ea8a1ede3284b925ca3ccb3ab8f2f
ft32mtelematica <ft32mtelematica@gmail.com>
2025-08-15 12:06:40 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -3,8 +3,9 @@ st.title("PREVISÃO DE RANCHO")
 
 pg = st.navigation([
     st.Page("pages/geral.py", title="Valores Diários", icon="📊"),
-    st.Page("pages/por_pessoa2.py", title="Valores por Pessoa", icon="👤"),
+    st.Page("pages/por_pessoa4.py", title="Valores por Pessoa", icon="👤"),
     st.Page("pages/fluxodecaixa.py", title="Fluxo de Caixa", icon=("💰")),
+    st.Page("pages/retiradas.py", title="Retiradas", icon="💸"),
 ])
 
 pg.run()

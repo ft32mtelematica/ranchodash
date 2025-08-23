6435f4ca1c16e83e422e550f10ad3b70bc8d13f4
ft32mtelematica <ft32mtelematica@gmail.com>
2025-08-22 21:44:49 -0300

    Update geral.py
    V5

--- pages/geral.py
+++ pages/geral.py
@@ -1,4 +1,24 @@
 import streamlit as st
 
-st.title(" Valores Diários")
-st.write(" Aqui você pode visualizar os valores diários de rancho.")
+flex = st.container(horizontal=True,)
+
+for card in range(3):
+    flex.button(f"Button {card + 1}")
+    
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    card1 = st.container(border=True, key=None, width="stretch", height="content", horizontal=True, horizontal_alignment="center", vertical_alignment="top", gap="small")
+    card1.write("This is a card with a border.")
+    
+with col2:
+    card2 = st.container(border=True, key=None, width="stretch", height="content", horizontal=True, horizontal_alignment="center", vertical_alignment="top", gap="small")
+    card2.write("This is a card with a border.")
+    
+with col3:
+    card3 = st.container(border=True, key=None, width="stretch", height="content", horizontal=True, horizontal_alignment="center", vertical_alignment="top", gap="small")
+    card3.write("This is a card with a border.")
+    
+

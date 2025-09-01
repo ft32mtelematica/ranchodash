e8c920a8aba197fa11d3f49861e1223141db81b2
ft32mtelematica <ft32mtelematica@gmail.com>
2025-09-01 17:37:41 -0300

    Update geral.py

--- pages/geral.py
+++ pages/geral.py
@@ -1,24 +1,29 @@
 import streamlit as st
+from utils.styling import apply_global_styles, render_card
 
-flex = st.container(horizontal=True,)
+apply_global_styles()
 
-for card in range(3):
-    flex.button(f"Button {card + 1}")
-    
-
+st.subheader("Visão Geral")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    card1 = st.container(border=True, key=None, width="stretch", height="content", horizontal=True, horizontal_alignment="center", vertical_alignment="top", gap="small")
-    card1.write("This is a card with a border.")
-    
-with col2:
-    card2 = st.container(border=True, key=None, width="stretch", height="content", horizontal=True, horizontal_alignment="center", vertical_alignment="top", gap="small")
-    card2.write("This is a card with a border.")
-    
-with col3:
-    card3 = st.container(border=True, key=None, width="stretch", height="content", horizontal=True, horizontal_alignment="center", vertical_alignment="top", gap="small")
-    card3.write("This is a card with a border.")
-    
+    render_card(
+        label="Total Arrecadado",
+        value="R$ 1.250,00",
+        content="Conteúdo do Card 1."
+    )
 
+with col2:
+    render_card(
+        label="Saldo Atual",
+        value="R$ 345,70",
+        content="Conteúdo do Card 2."
+    )
+
+with col3:
+    render_card(
+        label="Militares no Rancho",
+        value="42",
+        content="Conteúdo do Card 3."
+    )

4831b7cc9dbb993bd317cc7356bba10534e0f616
ft32mtelematica <ft32mtelematica@gmail.com>
2025-09-17 20:37:39 -0300

    Update styling.py

--- utils/styling.py
+++ utils/styling.py
@@ -19,10 +19,17 @@ def apply_global_styles():
         st.error(f"Arquivo de estilo não encontrado em: {css_file_path}. Estilos personalizados não serão aplicados.")
         
 
-
-def render_card(label, value, content):
-    """Renderiza um card completo com estilo injetado."""
+def render_card(label, value=None, content=None, chart=None, render_now=True):
+    """
+    Cria o HTML para um card de métrica.
     
+    Args:
+        label (str): O rótulo do card.
+        value (str): O valor principal do card.
+        content (str): O conteúdo descritivo do card.
+        render_now (bool): Se True, renderiza o card imediatamente com st.markdown. 
+                           Se False, retorna a string HTML do card.
+    """
     # CSS específico para o card. O padding é aplicado aqui.
     card_html = f"""
     <div style="
@@ -32,11 +39,14 @@ def render_card(label, value, content):
         margin-bottom: 15px;
         padding: 20px;
         box-sizing: border-box;
-        height: 100%;
+        height: 93%;
     ">
         <div style="font-family: Verdana, Geneva, sans-serif; color: #E0E0E0; font-size: 14px;">{label}</div>
         <div style="font-family: Arial Black, Gadget, sans-serif; color: white; font-size: 32px; margin: 10px 0;">{value}</div>
         <div style="font-family: Verdana, Geneva, sans-serif; color: #E0E0E0; font-size: 14px;">{content}</div>
     </div>
     """
-    st.markdown(card_html, unsafe_allow_html=True)
+    if render_now:
+        st.markdown(card_html, unsafe_allow_html=True)
+    else:
+        return card_html
